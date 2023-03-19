7e28785a5ae85c1d61569a618ba0b4ec68c6594d
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-03-19 13:12:51 -0600

    Piano
    Testing Piano

--- app.js
+++ app.js
@@ -6,47 +6,47 @@ var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Define the matrix characters
-var matrixCharacters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}[]|;':\",./<>?`~";
+// Define the music characters
+var musicCharacters = "♩♪♫♬♭♮♯";
 
-// Define the matrix columns
+// Define the music columns
 var columns = canvas.width / 20;
 
-// An array to store the matrix drops
+// An array to store the music drops
 var drops = [];
 
-// Create the matrix drops
+// Create the music drops
 for (var i = 0; i < columns; i++) {
   drops[i] = Math.floor(Math.random() * canvas.height);
 }
 
-// Draw the matrix animation
-function drawMatrix() {
+// Draw the music animation
+function drawmusic() {
   // Set the canvas background color to black
   ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   
-  // Set the font and color for the matrix characters
+  // Set the font and color for the music characters
   ctx.font = "15px monospace";
-  ctx.fillStyle = "#0F0";
+  ctx.fillStyle = "#FFF";
   
-  // Loop through the matrix columns
+  // Loop through the music columns
   for (var i = 0; i < columns; i++) {
-    // Get a random matrix character
-    var matrixCharacter = matrixCharacters[Math.floor(Math.random() * matrixCharacters.length)];
+    // Get a random music character
+    var musicCharacter = musicCharacters[Math.floor(Math.random() * musicCharacters.length)];
     
-    // Draw the matrix character at the current position
-    ctx.fillText(matrixCharacter, i * 20, drops[i] * 20);
+    // Draw the music character at the current position
+    ctx.fillText(musicCharacter, i * 20, drops[i] * 20);
     
-    // Move the matrix drop down by 1
+    // Move the music drop down by 1
     drops[i]++;
     
-    // If the matrix drop is outside the canvas, reset it to the top
+    // If the music drop is outside the canvas, reset it to the top
     if (drops[i] * 20 > canvas.height && Math.random() > 0.95) {
       drops[i] = 0;
     }
   }
 }
 
-// Call the drawMatrix function every 50 milliseconds
-setInterval(drawMatrix, 50);
+// Call the drawmusic function every 50 milliseconds
+setInterval(drawmusic, 50);
